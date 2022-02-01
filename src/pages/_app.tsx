43039d8976e682e169222b9b6a806b2cac84e0f3
Rafael Angonese <rafael.angonese09@gmail.com>
2022-02-01 19:27:01 -0300

    :sparkles: feat: config styled components theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import "../styles/globals.css";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
