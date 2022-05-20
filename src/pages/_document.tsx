de3ca394ca105c7f1a8b9af24a4a6b5d9421e368
Rafael Angonese <rafael.angonese09@gmail.com>
2022-05-20 01:01:52 -0300

    :lipstick: feat: add Fira Code font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,14 @@
-import Document, { DocumentContext } from "next/document";
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  //--------------For styled-components only------------//
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -26,5 +33,24 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  //---------------------------------------------------//
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
